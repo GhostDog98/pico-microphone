9010a1b30551ab8735774eddb7efda5b25512d0a
ghostoverflow256 <46546505+GhostDog98@users.noreply.github.com>
2023-04-27 14:35:35 +1000

    Delete build directory

--- build/CMakeFiles/data.dir/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/data.dir/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c.obj: \
- /Users/aronj23/pico-microphone/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.c \
- /Users/aronj23/pico-microphone/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/crc.h \
- /usr/local/Cellar/arm-none-eabi-gcc/10-2020-q4-major/gcc/lib/gcc/arm-none-eabi/10.2.1/include/stddef.h
